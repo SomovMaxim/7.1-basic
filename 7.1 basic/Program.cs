4b258377ea07fc7d644326c7bfc7d7c8cac0f3b5
Максим Сомов <catfishvip@mail.ru>
2020-12-23 22:30:59 +0200

    Добавьте файлы проекта.

---
+++ 7.1 basic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._1_basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "a, :, s, d, :, 2, 0, p, q, :";
+            Console.WriteLine(x.Replace(":", ";"));
+            Console.ReadKey();
+        }
+    }
+}
